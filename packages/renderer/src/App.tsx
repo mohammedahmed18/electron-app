c7d8ba8dd9e53cedae612c7e1ff1c23521e12f4b
mohammedahmed18 <mohammed18200118@gmail.com>
2022-10-10 11:51:41 +0200

    add tailwindcss and daisyui

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -2,22 +2,12 @@ import { useEffect } from "react";
 import { ipcRenderer } from "electron";
 import channels from "../../shared/lib/ipc-channels";
 import "./App.css";
-declare global {
-  interface Window {
-    __electron?: any;
-  }
-}
 function App() {
   useEffect(() => {
     // app is ready
     ipcRenderer.send(channels.APP_READY);
-    console.log("app is ready");
   }, []);
-  return (
-    <div className="App">
-      <h1>platform: {window.__electron.platform} </h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
